a53823b7dc895ce856c7a1f356148fa03fee7a75
realfull <58330328+realfull@users.noreply.github.com>
2021-02-05 19:53:56 +0800

    Add files via upload

---
+++ opyzf_wxpay_return.php
@@ -0,0 +1,42 @@
+<?php 
+require_once('./includes/common.php');
+require_once(SYSTEM_ROOT."epay/yzf_wxpay.php");
+require_once(SYSTEM_ROOT."epay/epay_notify.class.php");
+
+@header('Content-Type: text/html; charset=UTF-8');
+
+//计算得出通知验证结果
+$alipayNotify = new AlipayNotify($alipay_config);
+$verify_result = $alipayNotify->verifyReturn();
+if($verify_result) {
+	//商户订单号
+	$out_trade_no = $_GET['out_trade_no'];
+
+	//支付宝交易号
+	$trade_no = $_GET['trade_no'];
+
+	//交易状态
+	$trade_status = $_GET['trade_status'];
+
+    if($_GET['trade_status'] == 'TRADE_FINISHED' || $_GET['trade_status'] == 'TRADE_SUCCESS') {
+		$srow=$DB->query("SELECT * FROM pay_order WHERE trade_no='{$out_trade_no}' limit 1")->fetch();
+		$url=creat_callback($srow);
+		if($srow['status']==0){
+			$DB->query("update `pay_order` set `status` ='1',`endtime` ='$date' where `trade_no`='$out_trade_no'");
+			$addmoney=round($srow['money']*$conf['money_rate']/100,2);
+			$DB->query("update pay_user set money=money+{$addmoney} where id='{$srow['pid']}'");
+			echo '<script>window.location.href="'.$url['return'].'";</script>';
+		}else{
+			echo '<script>window.location.href="'.$url['return'].'";</script>';
+		}
+    }
+    else {
+      echo "trade_status=".$_GET['trade_status'];
+    }
+}
+else {
+    //验证失败
+	echo('验证失败！');
+}
+
+?>
